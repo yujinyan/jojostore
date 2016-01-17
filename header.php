d317c0baaae85cbce02dce7f2e2fca5efc721a2f
Jinyan Yu <yujinyan@outlook.com>
2016-01-17 20:31:14 +0800

    header 中添加 favicon.png

--- header.php
+++ header.php
@@ -13,6 +13,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.png" />
 
 <?php wp_head(); ?>
 </head>
